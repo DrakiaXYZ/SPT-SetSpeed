4f15322e9252015d226add305d0f00c23f8933e2
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-08-02 20:08:11 -0700

    Add project files.

---
+++ SetSpeedPlugin.cs
@@ -0,0 +1,103 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Comfort.Common;
+using EFT;
+using HarmonyLib;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace DrakiaXYZ.SetSpeed
+{
+    [BepInPlugin("xyz.drakia.setspeed", "DrakiaXYZ-SetSpeed", "1.0.0")]
+    public class SetSpeedPlugin : BaseUnityPlugin
+    {
+        Type _MovementStateType;
+        Type _MovementContextType;
+
+        FieldInfo _MovementContextField;
+
+        MethodInfo _CurrentManagedStateGetter;
+        MethodInfo _SetCharacterMovementSpeedMethod;
+        MethodInfo _RaiseChangeSpeedEvent;
+        MethodInfo _MaxSpeedGetter;
+
+        private void Awake()
+        {
+            Settings.Init(Config);
+            _CurrentManagedStateGetter = AccessTools.PropertyGetter(typeof(Player), "CurrentManagedState");
+            _MovementStateType = _CurrentManagedStateGetter.ReturnType;
+
+            _MovementContextField = AccessTools.Field(_MovementStateType, "MovementContext");
+            _MovementContextType = _MovementContextField.FieldType;
+
+            _SetCharacterMovementSpeedMethod = AccessTools.Method(_MovementContextType, "SetCharacterMovementSpeed");
+            _RaiseChangeSpeedEvent = AccessTools.Method(_MovementContextType, "RaiseChangeSpeedEvent");
+            _MaxSpeedGetter = AccessTools.PropertyGetter(_MovementContextType, "MaxSpeed");
+        }
+
+        public void Update()
+        {
+            if (!IsGameActive)
+            {
+                return;
+            }
+
+            if (IsKeyPressed(Settings.SetSpeed1Key.Value))
+            {
+                SetPlayerSpeed(Settings.Speed1.Value);   
+            }
+            if (IsKeyPressed(Settings.SetSpeed2Key.Value))
+            {
+                SetPlayerSpeed(Settings.Speed2.Value);
+            }
+            if (IsKeyPressed(Settings.SetSpeed3Key.Value))
+            {
+                SetPlayerSpeed(Settings.Speed3.Value);
+            }
+            if (IsKeyPressed(Settings.SetSpeed4Key.Value))
+            {
+                SetPlayerSpeed(Settings.Speed4.Value);
+            }
+            if (IsKeyPressed(Settings.SetSpeed5Key.Value))
+            {
+                SetPlayerSpeed(Settings.Speed5.Value);
+            }
+        }
+
+        private bool IsGameActive => Singleton<IBotGame>.Instantiated;
+
+        private void SetPlayerSpeed(int speed)
+        {
+            Player mainPlayer = Singleton<GameWorld>.Instance.MainPlayer;
+            object currentManagedState = _CurrentManagedStateGetter.Invoke(mainPlayer, new object[] { });
+            object movementContext = _MovementContextField.GetValue(currentManagedState);
+
+            // Get the max speed, so we can make a range of 0 - MaxSpeed
+            float maxSpeed = (float)_MaxSpeedGetter.Invoke(movementContext, new object[] { });
+            float finalSpeed = (speed / 100f) * maxSpeed;
+
+            _SetCharacterMovementSpeedMethod.Invoke(movementContext, new object[] { finalSpeed, false });
+            _RaiseChangeSpeedEvent.Invoke(movementContext, new object[] { });
+        }
+
+        // Custom KeyPressed check that handles modifiers, but also lets you hit more than one key at a time
+        private bool IsKeyPressed(KeyboardShortcut key)
+        {
+            if (!Input.GetKeyDown(key.MainKey))
+            {
+                return false;
+            }
+
+            foreach (var modifier in key.Modifiers)
+            {
+                if (!Input.GetKey(modifier))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,101 @@
+﻿using BepInEx.Configuration;
+using UnityEngine;
+
+namespace DrakiaXYZ.SetSpeed
+{
+    internal class Settings
+    {
+        private const string KeybindsSectionTitle = "Keybinds";
+        private const string SpeedsSectionTitle = "Speeds";
+
+        public static ConfigEntry<KeyboardShortcut> SetSpeed1Key;
+        public static ConfigEntry<KeyboardShortcut> SetSpeed2Key;
+        public static ConfigEntry<KeyboardShortcut> SetSpeed3Key;
+        public static ConfigEntry<KeyboardShortcut> SetSpeed4Key;
+        public static ConfigEntry<KeyboardShortcut> SetSpeed5Key;
+
+        public static ConfigEntry<int> Speed1;
+        public static ConfigEntry<int> Speed2;
+        public static ConfigEntry<int> Speed3;
+        public static ConfigEntry<int> Speed4;
+        public static ConfigEntry<int> Speed5;
+
+        public static void Init(ConfigFile Config)
+        {
+            SetSpeed1Key = Config.Bind(
+                KeybindsSectionTitle,
+                "Set Speed 1 Key",
+                new KeyboardShortcut(KeyCode.Keypad1),
+                "Set Speed to configured Speed 1");
+
+            SetSpeed2Key = Config.Bind(
+                KeybindsSectionTitle,
+                "Set Speed 2 Key",
+                new KeyboardShortcut(KeyCode.Keypad2),
+                "Set Speed to configured Speed 2");
+
+            SetSpeed3Key = Config.Bind(
+                KeybindsSectionTitle,
+                "Set Speed 3 Key",
+                new KeyboardShortcut(KeyCode.Keypad3),
+                "Set Speed to configured Speed 3");
+
+            SetSpeed4Key = Config.Bind(
+                KeybindsSectionTitle,
+                "Set Speed 4 Key",
+                new KeyboardShortcut(KeyCode.Keypad4),
+                "Set Speed to configured Speed 4");
+
+            SetSpeed5Key = Config.Bind(
+                KeybindsSectionTitle,
+                "Set Speed 5 Key",
+                new KeyboardShortcut(KeyCode.Keypad5),
+                "Set Speed to configured Speed 5");
+
+            Speed1 = Config.Bind(
+                SpeedsSectionTitle,
+                "Speed 1",
+                1,
+                new ConfigDescription(
+                    "The speed that Speed Key 1 sets movement to",
+                    new AcceptableValueRange<int>(0, 100)
+                ));
+
+            Speed2 = Config.Bind(
+                SpeedsSectionTitle,
+                "Speed 2",
+                1,
+                new ConfigDescription(
+                    "The speed that Speed Key 2 sets movement to",
+                    new AcceptableValueRange<int>(0, 100)
+                ));
+
+            Speed3 = Config.Bind(
+                SpeedsSectionTitle,
+                "Speed 3",
+                1,
+                new ConfigDescription(
+                    "The speed that Speed Key 3 sets movement to",
+                    new AcceptableValueRange<int>(0, 100)
+                ));
+
+            Speed4 = Config.Bind(
+                SpeedsSectionTitle,
+                "Speed 4",
+                1,
+                new ConfigDescription(
+                    "The speed that Speed Key 4 sets movement to",
+                    new AcceptableValueRange<int>(0, 100)
+                ));
+
+            Speed5 = Config.Bind(
+                SpeedsSectionTitle,
+                "Speed 5",
+                1,
+                new ConfigDescription(
+                    "The speed that Speed Key 5 sets movement to",
+                    new AcceptableValueRange<int>(0, 100)
+                ));
+        }
+    }
+}
